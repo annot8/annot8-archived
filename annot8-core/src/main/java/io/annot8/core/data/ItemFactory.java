bbb476a3325c5d451433290e1ac43dc79d24a00e
James Baker <jdbaker@dstl.gov.uk>
2019-09-10 13:52:20 +0100

    Fix documentation

--- annot8-core/src/main/java/io/annot8/core/data/ItemFactory.java
+++ annot8-core/src/main/java/io/annot8/core/data/ItemFactory.java
@@ -1,12 +1,10 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.core.data;
 
-import io.annot8.core.data.Item;
-
 /**
  * Factory to create new items.
  *
- * <p>The implementation must always be able to create a new item, but it doesn't not need to
+ * <p>The implementation must always be able to create a new item, but it doesn't need to
  * support linking with child items. In which case create(Item parent) should call create().
  */
 public interface ItemFactory {
@@ -19,7 +17,7 @@ public interface ItemFactory {
   Item create();
 
   /**
-   * Create a new item for a parent
+   * Create a new item with a parent
    *
    * @return non-null
    */
