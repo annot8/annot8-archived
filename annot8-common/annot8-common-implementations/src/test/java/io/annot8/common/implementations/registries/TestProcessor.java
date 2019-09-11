43b8a7a5043f970890795eef7a86ab9fc459accd
James Baker <jdbaker@dstl.gov.uk>
2019-09-11 13:31:46 +0100

    Fixes tests

--- annot8-common/annot8-common-implementations/src/test/java/io/annot8/common/implementations/registries/TestProcessor.java
+++ annot8-common/annot8-common-implementations/src/test/java/io/annot8/common/implementations/registries/TestProcessor.java
@@ -1,6 +1,8 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.common.implementations.registries;
 
+import io.annot8.core.capabilities.AbstractComponentCapabilities;
+import io.annot8.core.capabilities.ComponentCapabilities;
 import io.annot8.core.components.Processor;
 import io.annot8.core.components.responses.ProcessorResponse;
 import io.annot8.core.data.Item;
@@ -10,5 +12,10 @@ public class TestProcessor implements Processor {
   @Override
   public ProcessorResponse process(Item item) {
     return ProcessorResponse.ok();
+  }
+
+  @Override
+  public ComponentCapabilities getCapabilities() {
+    return new AbstractComponentCapabilities() {};
   }
 }
--- annot8-testing/annot8-test-impl/src/main/java/io/annot8/testing/testimpl/components/ItemCollector.java
+++ annot8-testing/annot8-test-impl/src/main/java/io/annot8/testing/testimpl/components/ItemCollector.java
@@ -1,12 +1,14 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.testing.testimpl.components;
 
-import java.util.LinkedList;
-import java.util.List;
-
+import io.annot8.core.capabilities.AbstractComponentCapabilities;
+import io.annot8.core.capabilities.ComponentCapabilities;
 import io.annot8.core.components.Processor;
 import io.annot8.core.components.responses.ProcessorResponse;
 import io.annot8.core.data.Item;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class ItemCollector implements Processor {
 
@@ -24,5 +26,10 @@ public class ItemCollector implements Processor {
 
   public void clear() {
     items.clear();
+  }
+
+  @Override
+  public ComponentCapabilities getCapabilities() {
+    return new AbstractComponentCapabilities() {};
   }
 }
