676538cf109cef691b1773d63a2f3b03d21ab4bd
Chris Flatley <chris@committed.io>
2019-09-13 09:38:13 +0100

    Fix formatting

--- annot8-common/annot8-common-implementations/src/test/java/io/annot8/common/implementations/context/SimpleContextTest.java
+++ annot8-common/annot8-common-implementations/src/test/java/io/annot8/common/implementations/context/SimpleContextTest.java
@@ -1,11 +1,12 @@
 /* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.common.implementations.context;
 
-import io.annot8.core.components.Resource;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.Test;
+
+import io.annot8.core.components.Resource;
 
 class SimpleContextTest {
 
