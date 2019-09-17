142e69c5008a8ab5adbbc1e1fce9f28e49fdf883
James Baker <jdbaker@dstl.gov.uk>
2019-09-17 14:39:08 +0100

    Retain information from SettingsClass at runtime

--- annot8-api/src/main/java/io/annot8/api/components/annotations/SettingsClass.java
+++ annot8-api/src/main/java/io/annot8/api/components/annotations/SettingsClass.java
@@ -3,11 +3,21 @@ package io.annot8.api.components.annotations;
 
 import io.annot8.api.settings.Settings;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 /**
  * Indicates the settings class used by a component.
  *
  * Should be used on the {@link io.annot8.api.components.Annot8ComponentDescriptor}
  */
+@Documented
+@Retention(RUNTIME)
+@Target(TYPE)
 public @interface SettingsClass {
   Class<? extends Settings> value();
 }
